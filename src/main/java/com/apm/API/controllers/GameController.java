b0c1ebe7da772eb6b02a723cca76c67bbdddb4ae
madelin <madelintania@gmail.com>
2023-06-25 01:09:14 -0300

    endpoint-edit-Game

--- src/main/java/com/apm/API/controllers/GameController.java
+++ src/main/java/com/apm/API/controllers/GameController.java
@@ -2,11 +2,15 @@ package com.apm.API.controllers;
 
 import com.apm.API.entities.Game;
 import com.apm.API.services.GameService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/game")
@@ -24,7 +28,20 @@ public class GameController {
     }
 
     @PutMapping("/edit")
-    public void editGame(@RequestBody Game game){
+    public void editGame(@RequestBody Game game) throws Exception{
+        gameService.editGame(game);
         
+    }
+    
+     @DeleteMapping("/delete")
+    public void deleteGame (@RequestParam Integer id) throws Exception {
+        gameService.deleteGame(id);
+    }
+    
+    
+    @GetMapping("/getAll")
+    public List<Game> getGames(){
+    
+     return  gameService.getGames();
     }
 }
