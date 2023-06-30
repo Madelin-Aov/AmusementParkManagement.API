d076f19efa104dbaea66a2ad0298797f32883cb7
madelin <madelintania@gmail.com>
2023-06-29 21:41:43 -0300

    create-employee-getAll-endpoint

---
+++ src/main/java/com/apm/API/controllers/EmployeeController.java
@@ -0,0 +1,41 @@
+
+package com.apm.API.controllers;
+
+
+import com.apm.API.entities.Employee;
+import com.apm.API.services.EmployeeService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/employee")
+
+@RestController
+public class EmployeeController {
+    
+    @Autowired
+    EmployeeService employeeService;
+    
+    @PostMapping("/create")
+    public void createEmployee(@RequestBody Employee employee) {
+        employeeService.createEmployee(employee);
+
+    }
+    
+     @DeleteMapping("/delete")
+     public void deleteEmployee(@RequestParam Integer id) throws Exception {
+        employeeService.deleteEmployee(id);
+    }
+    
+    @GetMapping("/getAll")
+    public List<Employee> getBuyer(){
+    
+     return  employeeService.getBuyer();
+    } 
+}
