0f4e4a3a66b64ac69da02c8da943d4d652ab26cd
madelin <madelintania@gmail.com>
2023-07-02 23:19:46 -0300

    create-ticket-getAll-endpoint

--- src/main/java/com/apm/API/controllers/TicketController.java
+++ src/main/java/com/apm/API/controllers/TicketController.java
@@ -3,7 +3,10 @@ package com.apm.API.controllers;
 import com.apm.API.dtos.TicketsDTO;
 import com.apm.API.entities.Ticket;
 import com.apm.API.services.TicketService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +21,20 @@ public class TicketController {
     TicketService ticketService;
     
     @PostMapping("/create")
-    public void createEmployee(@RequestBody TicketsDTO ticketsDTO) {
+    public void createTicket(@RequestBody TicketsDTO ticketsDTO) {
         ticketService.createTicket(ticketsDTO);
 
+    }
+    
+   @DeleteMapping("/delete")
+    public void deleteTicket(@RequestParam Integer id) throws Exception {
+        ticketService.deleteTicket(id);
+    }
+    
+     @GetMapping("/getAll")
+    public List<Ticket>getTickets(){
+    
+     return  ticketService.getTickets();
     }
     
 }
