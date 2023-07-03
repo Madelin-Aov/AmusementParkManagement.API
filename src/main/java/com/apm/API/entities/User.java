ce79db9cd00b945c69d5e3c855c24ba138db40a2
madelin <madelintania@gmail.com>
2023-07-03 11:57:07 -0300

    create-user-create-endpoint

--- src/main/java/com/apm/API/entities/User.java
+++ src/main/java/com/apm/API/entities/User.java
@@ -1,6 +1,10 @@
 
 package com.apm.API.entities;
+import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import lombok.Getter;
@@ -8,8 +12,10 @@ import lombok.Setter;
 
 @Getter @Setter
 @Entity
-public class User {
+public class User implements Serializable{
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+   
     private Integer id;
     private String password;
     private String userName;
