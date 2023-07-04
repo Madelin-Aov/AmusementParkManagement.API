1d7ab248d0876a73ee8d23d57ee9fd12640834dd
madelin <madelintania@gmail.com>
2023-07-04 19:02:46 -0300

    Add-ReadMe

--- src/main/java/com/apm/API/repositories/BuyerRepository.java
+++ src/main/java/com/apm/API/repositories/BuyerRepository.java
@@ -5,6 +5,7 @@ import com.apm.API.entities.Buyer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface BuyerRepository extends JpaRepository<Buyer, Integer>{
     
